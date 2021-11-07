47dde1e1d08822c627ca3306c3266163597acc77
TakoBigvava <bigvava0205@gmail.com>
2021-11-08 03:06:55 +0400

    feat:code-4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const path = require ('path');
+const src = path.join(__dirname, 'files');
+const dest = path.join(__dirname, 'files-copy');
 
+async function copyFolder(dest) {
+  fs.mkdir(dest, {recursive: true},(err)=>{
+    if(err){
+      return console.log(err);
+    }
+  });
+
+  const dir = await fs.promises.readdir(src);
+
+  for await (const file of dir) { 
+   
+    fs.stat(path.join(src, file), (err) => {
+      if (err) {
+        return console.log(err);
+      }
+      const filePath = path.join(dest, path.basename(file));
+      fs.copyFile(path.join(src, file), filePath, (err)=>{
+        if(err){
+          return console.log(err);
+        } 
+      });
+
+    });
+  }
+}
+copyFolder(dest);
