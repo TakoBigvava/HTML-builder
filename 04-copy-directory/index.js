42e945e0fd3fa7cdb13f645c78e836e67006f96b
TakoBigvava <bigvava0205@gmail.com>
2021-11-09 23:53:09 +0400

    fix:code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,18 +2,22 @@ const fs = require('fs');
 const path = require ('path');
 const src = path.join(__dirname, 'files');
 const dest = path.join(__dirname, 'files-copy');
-
+fs.mkdir(dest, {recursive: true},(err)=>{
+  if(err){
+    return console.log(err);
+  }
+});
 async function copyFolder(dest, src) {
   const destDir = await fs.promises.readdir(dest);
   for await (const file of destDir) { 
     const filePath = path.join(dest, path.basename(file));
     fs.unlink(filePath, (err)=>{if(err)throw err;}); 
   }
-  fs.mkdir(dest, {recursive: true},(err)=>{
-    if(err){
-      return console.log(err);
-    }
-  });
+  // fs.mkdir(dest, {recursive: true},(err)=>{
+  //   if(err){
+  //     return console.log(err);
+  //   }
+  // });
   const dir = await fs.promises.readdir(src);
   for await (const file of dir) { 
     const filePath = path.join(dest, path.basename(file));
